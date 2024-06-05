4e5cd63dd5ed91416c369b1360233687dd457ffc
deepgohil <monisha.g@somaiya.edu>
2024-06-05 16:04:57 +0000

    first commit

---
+++ login.js
@@ -0,0 +1,11 @@
+const nameprint=function (name,age) {
+    console.log(`my nanme is ${name} and age is ${age}`);
+}
+const genderp=function (gender) {
+    console.log(`Your gender is ${gender}`);
+}
+
+module.exports={
+    nameprint,
+    genderp
+}
